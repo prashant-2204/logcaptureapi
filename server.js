4b41e82ae43363b5d0f786953d61f729ccb54e1a
Prashant <97339424+prashant-2204@users.noreply.github.com>
2025-03-13 16:16:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const upload = multer({ dest: uploadDir + '/' });
 console.log("R2_ACCESS_KEY_ID:", process.env.R2_ACCESS_KEY_ID);
 console.log("R2_SECRET_ACCESS_KEY:", process.env.R2_SECRET_ACCESS_KEY);
 console.log("R2_ENDPOINT:", process.env.R2_ENDPOINT);
+console.log("R2_ENDPOINT:", process.env.MONGODB_URL);
 
 // Defensive check for credentials
 const accessKeyId = process.env.R2_ACCESS_KEY_ID;
